5754f4cd744e5dcf53ee20724bf44c40baad02ed
Alvaro Padilla <varoboss.ap@gmail.com>
2023-03-02 01:45:26 -0600

    Add styles to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,23 @@
+import { HiHeart } from "react-icons/hi2";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <span className="footer-text">Made with&nbsp;</span>
+      <span className="heart-icon">
+        <HiHeart className="heart-icon" />
+      </span>
+      <span className="footer-text">&nbsp;by&nbsp;</span>
+      <a
+        className="footer-text--copy"
+        href="https://github.com/ese-varo"
+        target="_blank"
+        rel="noreferrer"
+      >
+        @ese-varo
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
